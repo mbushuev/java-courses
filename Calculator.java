33d1a97ac80d7b2454e70d1f0e4e1debbd5253fe
Maksim Bushuev <m.bushuev@entirec.ru>
2016-01-05 20:43:39 +0300

    Realising calculator

--- Calculator.java
+++ Calculator.java
@@ -1,36 +1,76 @@
 /**
-	Class realising calculator
-*/
+ * Class realising calculator
+ */
 
 public class Calculator {
-	
-	/**
-		Result of calculating
-	*/
-	private int result;
-	
-	/**
-		Sum arguments
-		@param params is arguments of summing
-	*/
-	public void add(int ... params){
-		for (Integer param : params){
-			this.result += param;
-		}
-	}
-	
-	/**
-		Get result
-		@return is result of computing
-	*/
-	public int getResult(){
-		return this.result;
-	}
-	
-	/**
-		Clear computing result
-	*/
-	public void cleanResult(){
-		this.result = 0;
-	}
+
+    /**
+     * Result of calculating
+     */
+    private double result;
+
+    /**
+     * Sum arguments
+     *
+     * @param params is arguments of summing
+     */
+    public void add(int... params) {
+        for (Integer param : params) {
+            this.result += param;
+        }
+    }
+
+    /**
+     * Difference of arguments
+     */
+    public void diff(int... params) {
+        this.result = this.result - (params[0] - params[1]);
+    }
+
+    /**
+     * Division of arguments
+     */
+    public void div(int... params) {
+        if (result != 0)
+            this.result = this.result / params[0] / params[1];
+        else
+            this.result = params[0] / params[1];
+    }
+
+    /**
+     * Multiplication
+     */
+    public void multi(int... params) {
+        if (result != 0)
+            this.result = this.result * params[0] * params[1];
+        else
+            this.result = params[0] * params[1];
+    }
+
+    /**
+     * Exponentiation
+     */
+    public void pow(int param1, int param2) {
+        result = param2;
+        for (int i = 1; i < param1; i++) {
+            result *= param2;
+        }
+    }
+
+    /**
+     * Get result
+     *
+     * @return is result of computing
+     */
+    public double getResult() {
+        return this.result;
+    }
+
+    /**
+     * Clear computing result
+     */
+    public void cleanResult() {
+        System.out.println("Result is clean!");
+        this.result = 0;
+    }
 }
--- InteractRunner.java
+++ InteractRunner.java
@@ -1,28 +1,80 @@
 import java.util.Scanner;
 
 /**
-	Class for running calculator. Maintaining user enter.
-*/
+ * Class for running calculator. Maintaining user enter.
+ */
 
 public class InteractRunner {
-	public static void main(String[] args){
-		Scanner reader = new Scanner(System.in);
-		try{
-			Calculator calc = new Calculator();
-			String exit = "no";
-			while(!exit.equals("yes")){
-				System.out.println("Enter first arg: ");
-				String first = reader.next();
-				System.out.println("Enter second arg: ");
-				String second = reader.next();
-				calc.add(Integer.valueOf(first), Integer.valueOf(second));
-				System.out.println("Result: "+calc.getResult());
-				calc.cleanResult();
-				System.out.println("Exit: yes/no");
-				exit = reader.next();
-			}
-		}finally{
-			reader.close();
-		}
-	}
+    public static void main(String[] args) {
+        Scanner reader = new Scanner(System.in);
+        try {
+            Calculator calc = new Calculator();
+            String exit = "no";
+            while (!exit.equals("yes")) {
+                System.out.println("Enter first arg: ");
+                String first = reader.next();
+                System.out.println("Enter second arg: ");
+                String second = reader.next();
+                boolean makeChange = false;
+                while(!makeChange) {
+                    System.out.println("Change operation:");
+                    System.out.println("1. Sum");
+                    System.out.println("2. Difference");
+                    System.out.println("3. Multiplication");
+                    System.out.println("4. Division");
+                    System.out.println("5. Exponential");
+                    System.out.println("6. Exit");
+                    String change = reader.next();
+                    switch (change) {
+                        case "1": {
+                            calc.add(Integer.valueOf(first), Integer.valueOf(second));
+                            System.out.println("Sum is: " + calc.getResult());
+                            makeChange = true;
+                            break;
+                        }
+                        case "2": {
+                            calc.diff(Integer.valueOf(first), Integer.valueOf(second));
+                            System.out.println("Difference is: " + calc.getResult());
+                            makeChange = true;
+                            break;
+                        }
+                        case "3": {
+                            calc.multi(Integer.valueOf(first), Integer.valueOf(second));
+                            System.out.println("Multiplication is: " + calc.getResult());
+                            makeChange = true;
+                            break;
+                        }
+                        case "4": {
+                            calc.div(Integer.valueOf(first), Integer.valueOf(second));
+                            System.out.println("Result of division is: " + calc.getResult());
+                            makeChange = true;
+                            break;
+                        }
+                        case "5": {
+                            calc.pow(Integer.valueOf(first), Integer.valueOf(second));
+                            System.out.println("Result of exponential second^first is: " + calc.getResult());
+                            makeChange = true;
+                            break;
+                        }
+                        case "6": {
+                            System.out.println("Change exit, good bye!");
+                            return;
+                        }
+                        default: {
+                            System.out.println("Not change one operation");
+                        }
+                    }
+                }
+                System.out.println("Clean result or not? yes/no");
+                String clean = reader.next();
+                if (clean.equals("yes")){
+                    calc.cleanResult();
+                }
+                System.out.println("Exit: yes/no");
+                exit = reader.next();
+            }
+        } finally {
+            reader.close();
+        }
+    }
 }
